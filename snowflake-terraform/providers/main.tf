64c092edbc454ed0506a7ac28bb45d1c98b22cc8
Alexandra Berces <alexandra.berces@clearstrategy.ie>
2025-08-18 10:50:30 +0100

    Recreating tf files

---
+++ snowflake-terraform/providers/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "snowflakedb/snowflake"
+      version = ">= 2.0.0"
+    }
+  }
+}
+
+locals {
+  organization_name = "vjzpvuk"
+  account_name      = "pb11640"
+  private_key_path  = "~/.ssh/snowflake_tf_snow_key.p8"
+}
+
+provider "snowflake" {
+    account           = var.snowflake_account
+    user              = var.snowflake_user
+    role              = var.snowflake_role
+    private_key       = base64decode(var.snowflake_private_key)
+}
+
+variable "snowflake_account" {}
+variable "snowflake_user" {}
+variable "snowflake_role" {}
+variable "snowflake_private_key" {}
---
+++ snowflake-terraform/snowflake/main.tf
@@ -0,0 +1,16 @@
+resource "snowflake_database" "tf_db" {
+  name         = "TF_DEMO_DB"
+  is_transient = false
+}
+
+resource "snowflake_warehouse" "tf_warehouse" {
+  name                      = "TF_DEMO_WH"
+  warehouse_type            = "STANDARD"
+  warehouse_size            = "XSMALL"
+  max_cluster_count         = 1
+  min_cluster_count         = 1
+  auto_suspend              = 60
+  auto_resume               = true
+  enable_query_acceleration = false
+  initially_suspended       = true
+}
