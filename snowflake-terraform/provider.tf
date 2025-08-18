2499c39af91e673687984b249b414bce1c4c2dfc
Alexandra Berces <alexandra.berces@clearstrategy.ie>
2025-08-18 11:48:20 +0100

    Removing locals

--- snowflake-terraform/provider.tf
+++ snowflake-terraform/provider.tf
@@ -7,12 +7,6 @@ terraform {
   }
 }
 
-locals {
-  organization_name = "vjzpvuk"
-  account_name      = "pb11640"
-  private_key_path  = "~/.ssh/snowflake_tf_snow_key.p8"
-}
-
 provider "snowflake" {
     account           = var.snowflake_account
     user              = var.snowflake_user
